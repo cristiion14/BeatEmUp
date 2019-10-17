b6f84288899a5cef13a684d2d7641a85d46de30d
cristiion14 <cristi.ion1996@gmail.com>
2019-10-17 20:45:13 +0100

    Updating animations + Movement Scripts
    -duplicating anim
    -removing events
    -creating the states in the animator
    adding triggers + boolean for walking
    Creating a class for the tags

---
+++ BeatEmUp2/Assets/Scrpits/Player Scripts/CharacterAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour {
+
+    private Animator anim;
+	// Use this for initialization
+	void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+    
+    public void Walk(bool move)
+    {
+        anim.SetBool(AnimationTags.MOVEMENT, move);
+    }
+    public void Punch1()
+    {
+        anim.SetTrigger(AnimationTags.PUNCH_1_TRIGGER);
+    }
+    public void Punch2()
+    {
+        anim.SetTrigger(AnimationTags.PUNCH_2_TRIGGER);
+    }
+    public void Punch3()
+    {
+        anim.SetTrigger(AnimationTags.PUNCH_3_TRIGGER);
+    }
+    public void Kick1()
+    {
+        anim.SetTrigger(AnimationTags.KICK_1_TRIGGER);
+    }
+    public void Kick2()
+    {
+        anim.SetTrigger(AnimationTags.KICK_2_TRIGGER);
+    }
+   
+
+}
---
+++ BeatEmUp2/Assets/Scrpits/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //animator reference
+    private CharacterAnimation playerAnim;
+    private Rigidbody rb;
+    public float walkSpeed = 2f;
+    public float zSpeed = 1.5f;
+
+    private float rotationY = -90f;
+    private float rotationSpeed = 15f;
+
+	// Use this for initialization
+	void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerAnim = GetComponentInChildren<CharacterAnimation>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RotatePlayer();
+        AnimatePlayerWalk();
+        AnimatePlayerAttack();
+	}
+
+    void FixedUpdate()
+    {
+        //any phisics calculations goes in here
+        DetectMov();
+    }
+    void DetectMov()
+    {
+        rb.velocity = new Vector3(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) * (-walkSpeed), rb.velocity.y, Input.GetAxisRaw(Axis.VERTICAL_AXIS) * (-zSpeed));
+    }
+
+    void RotatePlayer()
+    {                 //if is moving to the right
+        if(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS)>0)
+        {
+            transform.rotation = Quaternion.Euler(0f, -Mathf.Abs(rotationY), 0);
+        }
+        else if(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS)<0)
+        {            //if going to the left
+            transform.rotation = Quaternion.Euler(0f, Mathf.Abs(rotationY), 0);
+        }
+        
+    }
+
+    void AnimatePlayerWalk()
+    {                   //if the player moves in either position ---> call animation
+        if(Input.GetAxisRaw(Axis.HORIZONTAL_AXIS) != 0 || Input.GetAxisRaw(Axis.VERTICAL_AXIS) !=0)
+        {
+            playerAnim.Walk(true);
+        }
+        else
+        {
+            playerAnim.Walk(false);
+        }
+    }
+    void AnimatePlayerAttack()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            playerAnim.Punch1();
+        }
+        else if(Input.GetKeyDown(KeyCode.K))
+        {
+            playerAnim.Punch2();
+        }
+    }
+}
