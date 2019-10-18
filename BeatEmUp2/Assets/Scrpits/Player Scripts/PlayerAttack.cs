f89fa59d6c028a6608273e740f050f5d7cb58fc7
cristiion14 <cristi.ion1996@gmail.com>
2019-10-18 01:20:07 +0100

    Updating combos using enum+switch cases

--- BeatEmUp2/Assets/Scrpits/Player Scripts/PlayerAttack.cs
+++ BeatEmUp2/Assets/Scrpits/Player Scripts/PlayerAttack.cs
@@ -10,26 +10,29 @@ public enum ComboState
     KICK_1,
     KICK_2
 }
-public class PlayerAttack : MonoBehaviour {
+public class PlayerAttack : MonoBehaviour
+{
     CharacterAnimation playerAnim;
     bool activateTimerToReset;
     float defaultComboTimer = 0.4f;
     float currentComboTimer;
 
     ComboState currentComboState;
-	// Use this for initialization
-	void Awake () {
+    // Use this for initialization
+    void Awake()
+    {
         playerAnim = GetComponentInChildren<CharacterAnimation>();
-	}
-	
+    }
+
     void Start()
     {
         currentComboTimer = defaultComboTimer;
         currentComboState = ComboState.NONE;
     }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         AnimatePlayerAttack();
         ResetComboState();
 
@@ -39,38 +42,41 @@ public class PlayerAttack : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.J))
         {
-            currentComboState++;    //move the combo state from none-punch1(arrayIndex)
-            activateTimerToReset = true;
-            currentComboTimer = defaultComboTimer;
-            
-            if(currentComboState == ComboState.PUNCH_1)
-            {
-                playerAnim.Punch1();
-            }
+            if (currentComboState == ComboState.PUNCH_3 || currentComboState == ComboState.KICK_1 || currentComboState == ComboState.KICK_2)
+                return;
+                
+                currentComboState++;    //move the combo state from none-punch1(arrayIndex)
+                activateTimerToReset = true;
+                currentComboTimer = defaultComboTimer;
 
-            if (currentComboState == ComboState.PUNCH_2)
-            {
-                playerAnim.Punch2();
-            }
+                switch (currentComboState)
+                {
+                    case ComboState.PUNCH_1:
+                        playerAnim.Punch1();
+                        break;
+                    case ComboState.PUNCH_2:
+                        playerAnim.Punch2();
+                        break;
+                    case ComboState.PUNCH_3:
+                        playerAnim.Punch3();
+                        break;
+                    
+                }
 
-            if (currentComboState == ComboState.PUNCH_3)
-            {
-                playerAnim.Punch3();
-            }
 
+            }
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                playerAnim.Kick1(); //1.25.45 YT
+            }
         }
-        if(Input.GetKeyDown(KeyCode.K))
-        {
-            playerAnim.Kick1();
-        }
-    }
 
     void ResetComboState()
     {
-        if(activateTimerToReset)
+        if (activateTimerToReset)
         {
             currentComboTimer -= Time.deltaTime;
-            if(currentComboTimer<=0)
+            if (currentComboTimer <= 0)
             {
                 //ran out of time 
                 currentComboState = ComboState.NONE;
@@ -81,3 +87,4 @@ public class PlayerAttack : MonoBehaviour {
 
     }
 }
+
